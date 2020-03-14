61cbbe784948dfeefaf061d5644a6d809e9ebc29
Ivan Phelps <ivanniothemannio@gmail.com>
2020-03-14 11:40:12 -0500

    Add project files.

---
+++ Syntactic_Sugar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Syntactic_Sugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caterpillarPreds = new List<string> { "Birds", "Groundhogs" };
+            var caterpillarPrey = new List<string> { "Leaves", "Microscopic bois" };
+            var caterpillar = new Bug("Squirmy", "Caterpillar",  caterpillarPreds, caterpillarPrey);
+            Console.WriteLine(caterpillar.FormalName);
+            Console.WriteLine(caterpillar.PreyList());
+            Console.WriteLine(caterpillar.PredatorList());
+            Console.WriteLine(caterpillar.Eat("Leaves"));
+            Console.ReadLine();            
+        }
+    }
+    
+}
